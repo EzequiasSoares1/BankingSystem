773cb65a9ec6d33f3acf5b8b8155beb1395f7a0b
Gabriel Divtech <gabriel.lira@divtech.com.br>
2024-08-08 00:32:20 -0300

    feat: add function create transaction history

--- src/main/java/com/accenture/academico/bankingsystem/services/TransactionHistoryService.java
+++ src/main/java/com/accenture/academico/bankingsystem/services/TransactionHistoryService.java
@@ -2,19 +2,22 @@ package com.accenture.academico.bankingsystem.services;
 
 import com.accenture.academico.bankingsystem.domain.transation_history.TransactionHistory;
 import com.accenture.academico.bankingsystem.dtos.transaction_history.TransactionHistoryRequestDTO;
-import com.accenture.academico.bankingsystem.repositories.TransactionRepository;
+import com.accenture.academico.bankingsystem.repositories.TransactionHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TransactionHistoryService {
-    private final TransactionRepository transactionRepository;
+    private final TransactionHistoryRepository transactionHistoryRepository;
+    private final AccountService accountService;
     public final void createTransactionHistory(TransactionHistoryRequestDTO transactionHistoryDTO){
         TransactionHistory transactionHistory = new TransactionHistory();
 
-        //transactionHistory.setAccount();
+        transactionHistory.setAccount(accountService.getById(transactionHistoryDTO.accountId()));
         transactionHistory.setTransactionType(transactionHistoryDTO.transactionType());
         transactionHistory.setAmount(transactionHistoryDTO.value());
+
+        transactionHistoryRepository.save(transactionHistory);
     }
 }
